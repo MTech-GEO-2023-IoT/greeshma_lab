4ed0cf68c83aa9313ef424ba8e8b6481fc438660
Greeshu2096 <126846352+Greeshu2096@users.noreply.github.com>
2023-03-31 11:35:21 +0530

    Create editedled.py

---
+++ editedled.py
@@ -0,0 +1,20 @@
+#Python Program to demonstrate GPIO Output in Raspberry Pi
+#(C)Sai Shibu
+#Import GPIO Library
+import RPi.GPIO as GPIO
+#Import time function Library
+import time
+#Configure GPIO in Raspberry Pi BCM Mode
+GPIO.setmode(GPIO.BCM) 
+#Configure GPIO Pin 17 as output
+GPIO.setup(17,GPIO.OUT) 
+#Set GPIO Pin to High
+i=0
+while i<20
+ GPIO.output(17,GPIO.HIGH)
+ #Wait for 1sec
+ time.sleep(1)
+ #Set GPIO Pin to Low
+ GPIO.output(17,GPIO.LOW)
+ time.sleep(1)
+  i= i+1
