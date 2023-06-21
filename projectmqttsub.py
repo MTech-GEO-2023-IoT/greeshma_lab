abafd1f646bd28c2d1966c635efaeb32482c4203
Greeshu2096 <126846352+Greeshu2096@users.noreply.github.com>
2023-06-21 08:45:54 +0530

    Update projectmqttsub.py

--- projectmqttsub.py
+++ projectmqttsub.py
@@ -1,3 +1,5 @@
+
+''''
 import paho.mqtt.client as paho
 import pymysql
 global mqttclient;
@@ -42,3 +44,22 @@ mqttclient.connect(broker, port, keepalive=1, bind_address="")
  
 mqttclient.on_connect = _on_connect
 mqttclient.loop_forever()
+''''
+
+#Python Program to input data to mysql database
+#Import pymysql module library
+import pymysql
+#Create a connection to MySQL Database 
+conn =pymysql.connect(database="greeshma",user="user",password="PASS",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, switch , its power data
+data={'topic':'t','sw1':P1, 'sw2':P2, 'sw3':P3, 'sw4':P4, 'Sensorvalue':50}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO sensordata(topic, Sensorvalue)VALUES(%(name)s,%(int);",data)
+#Close the cursor
+cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
